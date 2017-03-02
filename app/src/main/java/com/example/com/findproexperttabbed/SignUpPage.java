ef1e1524e22f8d7bb300d697f9e215ad73069c8e
ketan <ketakale321@gmail.com>
2017-03-03 01:15:32 +0530

    ketan

--- app/src/main/java/com/example/com/findproexperttabbed/SignUpPage.java
+++ app/src/main/java/com/example/com/findproexperttabbed/SignUpPage.java
@@ -121,7 +121,7 @@ public class SignUpPage extends AppCompatActivity {
                 public void onDateSet(DatePicker arg0,
                                       int arg1, int arg2, int arg3) {
                     // TODO Auto-generated method stub
-                    // arg1 = year
+                    // arg1 = yea
                     // arg2 = month
                     // arg3 = day
                     showDate(arg1, arg2 + 1, arg3);
