f7e8811844fb89f08bb013216575c0af52551fc8
ketan <ketakale321@gmail.com>
2017-03-03 00:46:49 +0530

    ketan

--- app/src/main/java/com/example/com/findproexperttabbed/SignUpPage.java
+++ app/src/main/java/com/example/com/findproexperttabbed/SignUpPage.java
@@ -134,7 +134,7 @@ public class SignUpPage extends AppCompatActivity {
     }
 
     public void getValues() {
-        //just get the values
+        //just get the val
         fnames = fname.getText().toString().trim();
         lnames = lname.getText().toString().trim();
         adds = address.getText().toString().trim();
